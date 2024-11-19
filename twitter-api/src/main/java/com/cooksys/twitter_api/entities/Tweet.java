4cb2eabb73376873a0d19ccc9f43b00b4b461216
Andrew <goodman.drew1@gmail.com>
2024-11-19 12:38:21 -0600

    completed user entity and embedded profile entity

--- twitter-api/src/main/java/com/cooksys/twitter_api/entities/Tweet.java
+++ twitter-api/src/main/java/com/cooksys/twitter_api/entities/Tweet.java
@@ -3,14 +3,11 @@ package com.cooksys.twitter_api.entities;
 import java.security.Timestamp;
 import java.util.List;
 
-import org.antlr.v4.runtime.misc.NotNull;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinColumns;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
@@ -40,12 +37,12 @@ public class Tweet {
 	
 	//foreign key
 	@ManyToOne
-	@JoinColumn(name = "inReplyTo", referencedColumnName = "tweet_id")
+	@JoinColumn(name = "inReplyTo")
 	private Tweet inReplyTo;
 	
 	//foreign key
 	@ManyToOne
-	@JoinColumn(name = "repostOf", referencedColumnName = "tweet_id")
+	@JoinColumn(name = "repostOf")
 	private Tweet repostOf;
 	
 	@ManyToMany
--- twitter-api/src/main/java/com/cooksys/twitter_api/entities/User.java
+++ twitter-api/src/main/java/com/cooksys/twitter_api/entities/User.java
@@ -1,12 +1,17 @@
 package com.cooksys.twitter_api.entities;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,21 +19,53 @@ import lombok.NoArgsConstructor;
 @Entity
 @NoArgsConstructor
 @Data
-@Table(name = "users")
+@Table(name = "users") // Custom table name to avoid conflicts with reserved SQL keywords.
 public class User {
-	@Id
-	@GeneratedValue
-	@Column(name = "user_id")
-	private Long id;
-	
-	
-	@ManyToMany(mappedBy = "usersWhoLiked") 
-	private List<Tweet> likedTweets;
-	
-	@ManyToMany(mappedBy = "mentionedUsers") 
-	private List<Tweet> userMentioned;
-	
-	
-	
 
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    private Timestamp joined;
+
+    private boolean deleted;
+
+    @Embedded
+    private Profile profile;
+
+	@OneToMany(mappedBy = "author") // Maps to the 'author' field in the Tweet entity.
+    private List<Tweet> authoredTweets; // List of tweets authored by this user.
+
+    @ManyToMany
+    @JoinTable(
+        name = "followers_following", // Explicitly naming join table as per ERD.
+        joinColumns = @JoinColumn(name = "follower_id"),
+        inverseJoinColumns = @JoinColumn(name = "following_id")
+    )
+    private List<User> following; // list of user objects that the current user instance is following
+
+    @ManyToMany(mappedBy = "following")
+    private List<User> followers;  // inverse side of self-referencing many to many relationship w user
+
+    @ManyToMany
+    @JoinTable(
+        name = "user_likes", // Explicitly naming join table as per ERD.
+        joinColumns = @JoinColumn(name = "user_id"),
+        inverseJoinColumns = @JoinColumn(name = "tweet_id")
+    )
+    private List<Tweet> likedTweets; // list of tweet objects that a user has liked
+
+    @ManyToMany
+    @JoinTable(
+        name = "user_mentions", // Explicitly naming join table as per ERD.
+        joinColumns = @JoinColumn(name = "user_id"),
+        inverseJoinColumns = @JoinColumn(name = "tweet_id")
+    )
+    private List<Tweet> mentionedTweets; // list of tweet objects where a user is mentioned
 }
