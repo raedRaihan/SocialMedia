618acf15445482260884999552d7779f98804c07
raedRaihan <raed.ali@mavs.uta.edu>
2024-11-19 13:30:17 -0600

    backup

---
+++ twitter-api/src/main/java/com/cooksys/twitter_api/entities/Hashtag.java
@@ -0,0 +1,34 @@
+package com.cooksys.twitter_api.entities;
+
+import java.security.Timestamp;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class Hashtag {
+	
+	@Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String label;
+
+    // @CreationTimestamp
+    private Timestamp firstUsed;
+    private Timestamp lastUsed;
+
+    // @UpdateTimestamp
+    @ManyToMany(mappedBy = "hashtags")
+    private List<Tweet> tweets;
+
+}
