12812ff5723a53391e6dccdeb74b3b5aee44ee93
Andrew <goodman.drew1@gmail.com>
2024-11-19 18:05:58 -0600

    added controller skeletons

---
+++ twitter-api/src/main/java/com/cooksys/twitter_api/controllers/HashtagController.java
@@ -0,0 +1,15 @@
+package com.cooksys.twitter_api.controllers;
+
+import org.springframework.web.bind.annotation.*;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/hashtags")
+public class HashtagController {
+
+    // Add constructor-based dependency injection for service here if needed
+
+    // TODO: Implement hashtag-related endpoints later
+
+}
---
+++ twitter-api/src/main/java/com/cooksys/twitter_api/controllers/TweetController.java
@@ -0,0 +1,17 @@
+package com.cooksys.twitter_api.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/tweets")
+public class TweetController {
+
+    // Add constructor-based dependency injection for service here if needed
+
+    // TODO: Implement tweet-related endpoints later
+
+}
