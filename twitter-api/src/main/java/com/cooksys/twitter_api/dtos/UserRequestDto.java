475b4d43356e2ad1b238019d438fab1261704e92
Helena Makendengue <HelenaMakens1013@gmail.com>
2024-11-20 11:56:25 -0600

    Update UserRequestDto.java

--- twitter-api/src/main/java/com/cooksys/twitter_api/dtos/UserRequestDto.java
+++ twitter-api/src/main/java/com/cooksys/twitter_api/dtos/UserRequestDto.java
@@ -6,6 +6,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 public class UserRequestDto {
-    private CredentialsRequestDto credentials;
-    private ProfileRequestDto profile;
+    private CredentialsDto credentials;
+    private ProfileDto profile;
 }
