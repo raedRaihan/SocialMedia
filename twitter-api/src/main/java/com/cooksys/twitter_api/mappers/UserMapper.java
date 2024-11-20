2a4c20f20cfefafa5d7fe182f8d73aae730072e2
Helena Makendengue <HelenaMakens1013@gmail.com>
2024-11-20 12:13:36 -0600

    Update UserMapper.java

--- twitter-api/src/main/java/com/cooksys/twitter_api/mappers/UserMapper.java
+++ twitter-api/src/main/java/com/cooksys/twitter_api/mappers/UserMapper.java
@@ -6,15 +6,16 @@ import com.cooksys.twitter_api.dtos.UserResponseDto;
 import com.cooksys.twitter_api.entities.User;
 import org.mapstruct.Mapper;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = { ProfileMapper.class, CredentialsMapper.class })
 public interface UserMapper {
+
+    // Converts a User entity to a UserResponseDto (GET || DELETE) for returning data to the client
+    @Mapping(target = "username", source = "credentials.username")
+    UserResponseDto entityToDto(User entity);
 
     // Converts a UserRequestDto (POST || PATCH) to a User entity for saving to the database
     User requestDtoToEntity(UserRequestDto dto);
-
-    // Converts a User entity to a UserResponseDto (GET || DELETE) for returning data to the client
-    UserResponseDto entityToDto(User entity);
-
+    
     // Converts a list of User entities to a list of UserResponseDto
     List<UserResponseDto> entitiesToDtos(List<User> entities);
 }
